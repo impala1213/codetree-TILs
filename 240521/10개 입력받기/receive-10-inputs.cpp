d91f6b1e505f293eda42558643b82bd8f4934b74
impala1213 <84124472+impala1213@users.noreply.github.com>
2024-05-21 11:34:12 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240521/10개 입력받기/receive-10-inputs.cpp
+++ 240521/10개 입력받기/receive-10-inputs.cpp
@@ -6,17 +6,24 @@ int main() {
     int numbers[10];
     for (int i = 0; i<10;i++){
         cin >> numbers[i];
-        
+        count += 1;
+        if (numbers[i] == 0){
+            for(i;i<10;i++){
+                numbers[i] = 0;
+
+            }
+            break;
+        }
     }
-    for(int i = 9; i >= 0; i--){
+    for(int i = count; i >= 0; i--){
         if(numbers[i] == 0){
             continue;
         }
         total += numbers[i];
-        count +=1;
     }
     cout << fixed;
     cout.precision(1);
+    count -= 1;
     cout << total << " " << float(total / count);
     return 0;
 }
