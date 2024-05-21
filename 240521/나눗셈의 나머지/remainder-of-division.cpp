0f213da3d2bd789c8593a4805da4ea098042d6d6
impala1213 <84124472+impala1213@users.noreply.github.com>
2024-05-21 14:11:49 +0900

    Title: 나눗셈의 나머지, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240521/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a , b;
+    int remainder;
+    int list_remainder[10];
+    int total = 0;
+    for(int i = 0; i<10;i++){
+        list_remainder[i] = 0;
+    }
+    cin >> a;
+    cin >> b;
+    while(a>=1){
+        remainder = a%b;
+        a = a/b;
+        list_remainder[remainder] += 1;
+    }
+    for(int i = 0; i<10; i++){
+        
+        total += list_remainder[i] * list_remainder[i];
+    }
+    cout << total;
+    return 0;
+}
