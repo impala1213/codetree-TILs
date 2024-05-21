f9043f28bd561d9a2a77b4e4ab0dd9e061e2e464
impala1213 <84124472+impala1213@users.noreply.github.com>
2024-05-21 13:38:50 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240521/10개 입력받기/receive-10-inputs.cpp
+++ 240521/10개 입력받기/receive-10-inputs.cpp
@@ -12,7 +12,7 @@ int main() {
         }
         count += 1;
     }
-    for(int i = count; i >= 0; i--){
+    for(int i = count-1; i >= 0; i--){
         if(numbers[i] == 0){
             continue;
         }
