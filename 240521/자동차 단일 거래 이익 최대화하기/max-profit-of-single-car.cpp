dad495f3bb370112b13cad41a9ba1d77737df242
impala1213 <84124472+impala1213@users.noreply.github.com>
2024-05-21 14:50:50 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240521/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
+++ 240521/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -5,26 +5,22 @@ int main() {
     cin >> year;
     int price[year];
     int low,high = 0;
+    int profit = 0;
     for(int i = 0; i<year;i++){
         cin >> price[i];
     }
     low = price[0];
-    high = price[0];
     for(int i = 1; i<year;i++){
-        if(price[i] < low){
+        high = price[i];
+        if(low > price[i]){
             low = price[i];
-            high = low;
-        }
-        if(price[i]>high){
             high = price[i];
         }
+        if(high - low > profit){
+            profit = high - low;
+        }
     }
-    if(high == low){
-        cout << 0;
-    }
-    else{
-        cout << high - low;
-    }
+    cout << profit;
 
     return 0;
 }
